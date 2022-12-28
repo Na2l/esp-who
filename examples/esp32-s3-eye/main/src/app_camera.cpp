1f7686b4adeaa4cb2038bb6604f4e0bd64ea0a07
NaAl <h20@alocreative.com>
2022-12-28 11:01:32 -0700

    english

--- examples/esp32-s3-eye/main/src/app_camera.cpp
+++ examples/esp32-s3-eye/main/src/app_camera.cpp
@@ -63,7 +63,8 @@ AppCamera::AppCamera(const pixformat_t pixel_fromat,
     }
 
     sensor_t *s = esp_camera_sensor_get();
-    s->set_vflip(s, 1); // flip it back
+    //s->set_vflip(s, 1); // flip it back
+    s->set_hmirror(s, 1);
     // initial sensors are flipped vertically and colors are a bit saturated
     if (s->id.PID == OV3660_PID)
     {
